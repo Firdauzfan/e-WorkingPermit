8b1f13116eff54581f18ef9050c9e37fc9c5fdfe
firdauzfan <firdauzfanani@gmail.com>
2018-06-29 16:23:29 +0700

    fixing bugs upload image

--- uploadjpg.php
+++ uploadjpg.php
@@ -12,7 +12,7 @@
 $unikName 	= gmdate('ymdHis',time()+60*60*7);
 $fileup = $unikName. '.jpg';
 
-move_uploaded_file($_FILES['webcam']['tmp_name'],'vendor/'.$fileup)
+move_uploaded_file($_FILES['webcam']['tmp_name'],'vendor/'.$fileup);
 
 // print $success ? $file : 'Unable to save the file.';
 ?>
